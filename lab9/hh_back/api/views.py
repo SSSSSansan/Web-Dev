dbb5eb2ffaeb90e72848c7310eb398be599887a8
Saniya <erenkyzy@gmail.com>
2025-03-28 12:20:16 +0500

    lab9

---
+++ lab9/hh_back/api/views.py
@@ -0,0 +1,57 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Company, Vacancy
+from .serializers import CompanySerializer, VacancySerializer
+
+# 1. Список всех компаний
+@api_view(['GET'])
+def company_list(request):
+    companies = Company.objects.all()
+    serializer = CompanySerializer(companies, many=True)
+    return Response(serializer.data)
+
+# 2. Получение одной компании
+@api_view(['GET'])
+def company_detail(request, id):
+    try:
+        company = Company.objects.get(id=id)
+    except Company.DoesNotExist:
+        return Response({'error': 'Company not found'}, status=404)
+    serializer = CompanySerializer(company)
+    return Response(serializer.data)
+
+# 3. Список вакансий компании
+@api_view(['GET'])
+def company_vacancies(request, id):
+    try:
+        company = Company.objects.get(id=id)
+    except Company.DoesNotExist:
+        return Response({'error': 'Company not found'}, status=404)
+    vacancies = company.vacancies.all()
+    serializer = VacancySerializer(vacancies, many=True)
+    return Response(serializer.data)
+
+# 4. Список всех вакансий
+@api_view(['GET'])
+def vacancy_list(request):
+    vacancies = Vacancy.objects.all()
+    serializer = VacancySerializer(vacancies, many=True)
+    return Response(serializer.data)
+
+# 5. Получение одной вакансии
+@api_view(['GET'])
+def vacancy_detail(request, id):
+    try:
+        vacancy = Vacancy.objects.get(id=id)
+    except Vacancy.DoesNotExist:
+        return Response({'error': 'Vacancy not found'}, status=404)
+    serializer = VacancySerializer(vacancy)
+    return Response(serializer.data)
+
+# 6. Топ-10 вакансий по убыванию зарплаты
+@api_view(['GET'])
+def top_ten_vacancies(request):
+    vacancies = Vacancy.objects.order_by('-salary')[:10]
+    serializer = VacancySerializer(vacancies, many=True)
+    return Response(serializer.data)
